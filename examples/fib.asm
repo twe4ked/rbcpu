24f44e800f46b3c75bd40d6306ffaff3bca3c0a0
Odin Dutton <odindutton@gmail.com>
2016-06-13 12:47:44 +1000

    Fix fib.asm comments

--- examples/fib.asm
+++ examples/fib.asm
@@ -1,9 +1,10 @@
-; => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+; => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, nil, ..., nil, 11, 144, 89, 0, 144]
 ;
 ; 30: "counter"
 ; 31: "parent"
 ; 32: "grandparent"
 ; 33: "loop"
+; 34: "current"
 
 LDA #1
 STO #0
@@ -28,10 +29,10 @@ STO #34   ; store at "current"
 LDA 31    ; load "parent"
 STO #32   ; store in "grandparent"
 
-LDA 34    ; load the number at "counter"
+LDA 34    ; load the number at "current"
 STO #31   ; store the current number at "parent"
 
 LDA 33    ; load "loop"
 SUB #1    ; decrement "loop"
 STO #33   ; store in "loop"
-JNZ #19   ; jump to line 19 until "loop" is 0
+JNZ #20   ; jump to line 20 until "loop" is 0
